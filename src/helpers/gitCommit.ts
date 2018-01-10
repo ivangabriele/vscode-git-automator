d44c50dee4779c9b9cfc9a0e65657fa1419d20fb
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-01-10 19:17:17 +0100

    helpers/gitCommit: remove useless double quotes

--- src/helpers/gitCommit.ts
+++ src/helpers/gitCommit.ts
@@ -4,7 +4,7 @@ export default async function(message: string): Promise<any> {
   message = message.replace(/"/g, '\"')
 
   const command = 'git'
-  const args = ['commit', '-m', `"${message}"`]
+  const args = ['commit', '-m', `${message}`]
 
   return exec(command, args)
 }
