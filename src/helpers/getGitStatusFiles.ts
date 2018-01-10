dcd73ef5952e3dc4616cac85aae76226fe5f61c6
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-01-10 17:37:07 +0100

    helpers/getGitStatusFiles: create

---
+++ src/helpers/getGitStatusFiles.ts
@@ -0,0 +1,57 @@
+import * as path from 'path'
+import * as vscode from 'vscode'
+
+import gitStatus from './gitStatus'
+
+import { GitStatusFile } from '../types'
+
+interface GitShortActions {
+  [action: string]: GitStatusFile['state']
+}
+
+const GIT_SHORT_ACTIONS: GitShortActions = {
+  A: 'ADDED',
+  D: 'DELETED',
+  M: 'MODIFIED',
+  R: 'RENAMED'
+}
+
+export default async function(): Promise<GitStatusFile[]> {
+  const files: GitStatusFile[] = []
+  const workspaceRootAbsolutePath = vscode.workspace.workspaceFolders[0].uri.fsPath
+
+  let gitStatusStdOut: string
+  try {
+    gitStatusStdOut = await gitStatus()
+  }
+  catch (err) {
+    console.error(err)
+  }
+
+  return gitStatusStdOut.match(/[^\r\n]+/g)
+    .reduce((linesPartial: GitStatusFile[], line: string) => {
+      if (line.length === 0) return linesPartial
+
+      const reg = line[0] === 'R' ? /^(\w)\s+(.*)(?=\s->\s|$)(\s->\s)(.*)/ : /^(\w)\s+(.*)/
+      const regRes = line.match(reg)
+
+      if (regRes === null || regRes.length !== 3 && regRes.length !== 5) return linesPartial
+
+      linesPartial.push(
+        Object.assign(
+          {
+            path: regRes[2],
+            state: GIT_SHORT_ACTIONS[regRes[1]]
+          },
+          line[0] === 'R'
+            ? {
+              oldPath: regRes[2],
+              path: regRes[4]
+            }
+            : undefined
+        )
+      )
+
+      return linesPartial
+    }, [])
+}
