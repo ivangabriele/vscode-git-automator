7fb3674ecb8bcd805651f5f9474459ef4800bcfa
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-01-10 18:32:24 +0100

    extension: integrate settings load

--- src/extension.ts
+++ src/extension.ts
@@ -2,20 +2,25 @@ import * as vscode from 'vscode'
 import * as path from 'path'
 
 import addAndCommitFiles from './libs/addAndCommitFiles'
+import loadLocalConfig from './libs/loadLocalConfig'
+
+import { Settings } from './types'
 
 const workspaceRootAbsolutePath = vscode.workspace.workspaceFolders[0].uri.fsPath
 
 export function activate(context: vscode.ExtensionContext) {
+  const settings: Settings = loadLocalConfig(workspaceRootAbsolutePath)
+
   const addAndCommitAllFilesDisposable = vscode.commands.registerCommand(
     'extension.addAndCommitAllFiles',
-    () => addAndCommitFiles(['*'])
+    () => addAndCommitFiles(['*'], settings)
   )
 
   let addAndCommitCurrentFileDisposable = vscode.commands.registerCommand(
     'extension.addAndCommitCurrentFile',
     () => addAndCommitFiles([
       path.relative(workspaceRootAbsolutePath, vscode.window.activeTextEditor.document.fileName)
-    ])
+    ], settings)
   )
 
   context.subscriptions.push(addAndCommitAllFilesDisposable, addAndCommitCurrentFileDisposable)
