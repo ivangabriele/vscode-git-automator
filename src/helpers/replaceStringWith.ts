825b9bb71a931712c508e8b8524487e26993c4f0
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-01-11 16:03:10 +0100

    helpers/replaceStringWith: simplify

--- src/helpers/replaceStringWith.ts
+++ src/helpers/replaceStringWith.ts
@@ -2,16 +2,6 @@ import { Settings, SettingsPattern } from '../types';
 
 export default function replaceStringWith(str: string, patterns: SettingsPattern[]): string {
   return patterns.reduce((reducedStr: string, { pattern, with: val }: SettingsPattern): string => {
-    if (
-      pattern.length > 1
-      && pattern.substr(0, 1) === '/'
-      && pattern.substr(pattern.length - 1, 1) === '/'
-    ) {
-      const reg = new RegExp(pattern.substr(1, pattern.length - 2))
-
-      return reducedStr.replace(reg, val)
-    }
-
     return reducedStr.replace(pattern, val)
   }, str)
 }
