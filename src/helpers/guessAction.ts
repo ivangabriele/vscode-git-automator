25eed09d8c46b7464ae8341a4bcbdb5d987a2759
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-01-11 16:59:20 +0100

    helpers/guessAction: create

---
+++ src/helpers/guessAction.ts
@@ -0,0 +1,38 @@
+import { compose, find, prop, propEq } from 'ramda'
+
+import { FileGitState, GuessCustomAction } from '../types';
+
+export default function(commitMessage: string, state, customActions: GuessCustomAction[]): string {
+  const customAction = find(
+    ({ pattern: actionPattern, state: actionState }: GuessCustomAction) => {
+      if (actionState !== state) return false
+
+      return typeof actionPattern === 'string'
+        ? commitMessage.includes(actionPattern)
+        : actionPattern.test(commitMessage)
+    }
+  )(customActions)
+
+  if (customAction === undefined) {
+    switch (state) {
+      case 'ADDED':
+        commitMessage += 'create'
+        break
+
+      case 'DELETED':
+        commitMessage += 'remove'
+        break
+
+      case 'RENAMED':
+        commitMessage += 'move'
+        break
+
+      default:
+        break
+    }
+
+    return commitMessage
+  }
+
+  return commitMessage += customAction.with
+}
