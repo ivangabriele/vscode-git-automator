1d7066083fd90318240b9f4874fcc42698bfe9f5
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-01-11 05:34:26 +0100

    src/helpers/getCommonPathOfFiles: create

---
+++ src/helpers/getCommonPathOfFiles.ts
@@ -0,0 +1,23 @@
+export default function(...filesPaths: string[]): string {
+  let commonPath
+
+  filesPaths.forEach((filePath, index) => {
+    if (index === 0) return commonPath = filePath
+    if (commonPath.length === 0) return
+
+    let length = commonPath.length + 1
+    while (--length > 0) {
+      if (filePath.substr(0, length) === commonPath.substr(0, length)) break
+    }
+
+    if (length === 0) return
+    commonPath = commonPath.substr(0, length)
+  })
+
+  // Remove the trailing slash
+  if (commonPath.length !== 0 && commonPath[commonPath.length - 1] === '/') {
+    commonPath = commonPath.substr(0, commonPath.length - 2)
+  }
+
+  return commonPath
+}
