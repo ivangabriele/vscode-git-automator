2617464dbe9b1d7bb3b8b1f28cae1c2cbd49b130
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-11-01 23:52:27 +0100

    test/extension.test: create

---
+++ test/extension.test.ts
@@ -0,0 +1,34 @@
+import * as assert from 'assert'
+import * as fs from 'fs-extra'
+import * as path from 'path'
+
+import exec from '../src/helpers/exec'
+
+suite("Heroku Extension Tests", () => {
+  const fixturesPath = path.join(__dirname, 'fixtures')
+  const fixturesSourcePath = path.join(__dirname, '..', '..', 'src', 'test', 'fixtures')
+
+  suiteSetup(() => {
+    fs.copySync(path.join(fixturesSourcePath, 'sample.md'), path.join(fixturesPath, 'sample.md'))
+  })
+
+  test("Test helpers/exec()", async () => {
+    let command, args
+    switch (process.platform) {
+      case 'win32':
+      command = 'type'
+      break
+
+      default:
+      command = 'cat'
+      break
+    }
+
+    let o, x
+    try { o = await exec(command, [path.join(fixturesPath, 'sample.md')]) }
+    catch(e) { x = e }
+
+    assert.strictEqual(undefined, x)
+    assert.strictEqual(`# Hello World !\n`, o)
+  })
+})
