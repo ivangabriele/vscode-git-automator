b25c87d40b6c3797409eff7ea11d7402f4c39a04
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-11-03 00:31:52 +0100

    libs/pushLocalCommits: add TODO

--- src/libs/pushLocalCommits.ts
+++ src/libs/pushLocalCommits.ts
@@ -14,6 +14,7 @@ export default async function pushLocalCommits(settings: Settings): Promise<void
   const [err] = await to(showProgressNotification<void>('Pushing your local commits...', gitPush))
   // Git warnings are also caught here, so let's ignore them
   // TODO Fix the "To " with rejected commmits case
+  // TODO Handle the "Everything up-to-date" message
   if (typeof err !== 'string' || !/^(warning|to\s)/i.test(err)) {
     vscode.window.showErrorMessage(err)
     console.error(err)
