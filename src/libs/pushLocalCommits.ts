a9157bbc1a1d04a16092bda60d954f46fffd2a37
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-11-03 23:58:11 +0100

    libs/pushLocalCommits: fix rejected commits case

--- src/libs/pushLocalCommits.ts
+++ src/libs/pushLocalCommits.ts
@@ -11,11 +11,13 @@ export default async function pushLocalCommits(settings: Settings): Promise<void
   // ----------------------------------
   // GIT PUSH
 
-  const [err] = await to(showProgressNotification<void>('Pushing your local commits...', gitPush))
+  const [err, out] = await to(showProgressNotification<string>('Pushing your local commits...', gitPush))
+  console.log(err)
+  console.log(out)
+
   // Git warnings are also caught here, so let's ignore them
-  // TODO Fix the "To " with rejected commmits case
   // TODO Handle the "Everything up-to-date" message
-  if (typeof err !== 'string' || !/^(warning|to\s)/i.test(err)) {
+  if (typeof err !== 'string' || !(/^to\s/i.test(err) && !/!\s\[rejected\]/i.test(err))) {
     vscode.window.showErrorMessage(err)
     console.error(err)
 
