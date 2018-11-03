62b1b0980e57fb37487fef274ac49a407afc9731
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-11-04 00:11:13 +0100

    libs/pushLocalCommits: remove useless var

--- src/libs/pushLocalCommits.ts
+++ src/libs/pushLocalCommits.ts
@@ -11,7 +11,7 @@ export default async function pushLocalCommits(settings: Settings): Promise<void
   // ----------------------------------
   // GIT PUSH
 
-  const [err, out] = await to(showProgressNotification<string>('Pushing your local commits...', gitPush))
+  const [err] = await to(showProgressNotification<string>('Pushing your local commits...', gitPush))
 
   // Git warnings are also caught here, so let's ignore them
   if (typeof err !== 'string' || !(/^to\s/i.test(err) && !/!\s\[rejected\]/i.test(err))) {
