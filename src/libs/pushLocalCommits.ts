9cf360e6abda255e6c28b3fa282040af2e158ae3
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-11-04 00:10:56 +0100

    libs/pushLocalCommits: handle up-to-date case

--- src/libs/pushLocalCommits.ts
+++ src/libs/pushLocalCommits.ts
@@ -12,16 +12,17 @@ export default async function pushLocalCommits(settings: Settings): Promise<void
   // GIT PUSH
 
   const [err, out] = await to(showProgressNotification<string>('Pushing your local commits...', gitPush))
-  console.log(err)
-  console.log(out)
 
   // Git warnings are also caught here, so let's ignore them
-  // TODO Handle the "Everything up-to-date" message
   if (typeof err !== 'string' || !(/^to\s/i.test(err) && !/!\s\[rejected\]/i.test(err))) {
-    vscode.window.showErrorMessage(err)
-    console.error(err)
+    if (err === 'Everything up-to-date') {
+      vscode.window.showInformationMessage(err)
+    } else {
+      vscode.window.showErrorMessage(err)
+      console.error(err)
 
-    return
+      return
+    }
   }
 
   // ----------------------------------
